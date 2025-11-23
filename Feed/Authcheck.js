1c9706187a91d5ca699380aef18f28b4f5a75c8e
Lestro Farhan <farhaneditztfg@gmail.com>
2025-11-23 05:58:11 +0500

    deleted

--- Feed/Authcheck.js
+++
@@ -1,5 +0,0 @@
-let userLoggedin = JSON.parse(localStorage.getItem("currentUser"))
-
-if (!userLoggedin) {
-    window.location = "../auth/login.html"
-}
--- app.js
+++
@@ -1,7 +0,0 @@
-let userLoggedin = JSON.parse(localStorage.getItem("currentUser"))
-
-if (userLoggedin) {
-     window.location = "./Feed/Feed.html"
-} else {
-    window.location = "./auth/login.html"
-}
